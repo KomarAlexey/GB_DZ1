ae6b0763ba2a690d7445eaacbfb4f0ffc270527a
Alex <angrylexa@mail.ru>
2022-08-10 00:11:23 +0300

    DZ1

--- Program.cs
+++ Program.cs
@@ -1,8 +1,25 @@
 ﻿
-Console.WriteLine("Hello, World!");
-//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-//a = 5; b = 7 -> max = 7
-//a = 2 b = 10 -> max = 10
-//a = -9 b = -3 -> max = -3
+// Console.WriteLine("Hello, World!");
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
 
-Console.WriteLine()
+// Console.Clear();
+// Console.Write("Введите А: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите В: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// { 
+//       Console.WriteLine("А больше В");
+// }
+// else
+// {
+//     if (num1 < num2)
+//     {
+//         Console.WriteLine("В больше А");
+//     }
+// }
+
